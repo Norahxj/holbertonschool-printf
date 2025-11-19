480ecfbc75399089856e29918e3f1f8c6f31a234
Randa-hb10 <rm-400000@hotmail.com>
2025-11-19 08:23:09 +0000

    fixed some of the prototypes of functions to the main.h file

--- main.h
+++ main.h
@@ -1,10 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <unistd.h>
 
-int _printf(const char *int _printf(const char *format, ...);
+int _printf(const char *format, ...);
 int _putchar(char c);
-int print_string(char *str);
-int print_number(int n);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(void);
+
 #endif
