c794d52fdef8891f6dea9bd8417deba1b43f5ee9
Randa-hb10 <rm-400000@hotmail.com>
2025-11-23 08:17:09 +0000

    Add function prototypes and supporting functions for _printf

--- _printf.c
+++ _printf.c
@@ -18,9 +18,19 @@ while (*format)
 {
 if (*format == '%')
 {
+int plus_flag = 0, space_flag = 0, hash_flag = 0;
 format++;
 if (*format == '\0')
 return (-1);
+
+/* Read flags */
+while (*format == '+' || *format == ' ' || *format == '#')
+{           
+if (*format == '+') plus_flag = 1;
+else if (*format == ' ') space_flag = 1;
+else if (*format == '#') hash_flag = 1;
+format++;
+}
 if (*format == 'c')
 count += _putchar(va_arg(args, int));
 
@@ -31,21 +41,42 @@ else if (*format == '%')
 count += _putchar('%');
 
 else if (*format == 'd' || *format == 'i')
-count += print_number(va_arg(args, int));
+{
+int n = va_arg(args, int);
+if (n >= 0)
+{
+if (plus_flag) count += _putchar('+');
+else if (space_flag) count += _putchar(' ');
+}
+count += print_number(n);
+}
+
 else if (*format == 'b')
 count += print_binary(va_arg(args, unsigned int));
+
 else if (*format == 'u')
-	count += print_unsigned(va_arg(args, unsigned int));
+count += print_unsigned(va_arg(args, unsigned int));
+
 else if (*format == 'o')
-	count += print_octal(va_arg(args, unsigned int));
-else if (*format == 'x')
-	count += print_hex(va_arg(args, unsigned int));
-else if (*format == 'X')
-	count += print_HEX(va_arg(args, unsigned int));
+{
+unsigned int n = va_arg(args, unsigned int);
+if (hash_flag) count += _putchar('0');
+count += print_octal(n);
+}
+
+else if (*format == 'x' || *format == 'X')
+{
+unsigned int n = va_arg(args, unsigned int);
+if (hash_flag) { count += _putchar('0'); count += _putchar(*format); }
+if (*format == 'x') count += print_hex(n);
+else count += print_HEX(n);
+}
+
 else if (*format == 'S')
-	count += print_S(va_arg(args, char *));
+count += print_S(va_arg(args, char *));
+
 else if (*format == 'p')
-	count += print_pointer(va_arg(args, void *));
+count += print_pointer(va_arg(args, void *));
 
 else
 {
@@ -55,11 +86,11 @@ count += _putchar(*format);
 }
 else
 count += _putchar(*format);
+
 format++;
 }
 
-_putchar(-1);
-
+_putchar(-1); /* flush buffer */
 va_end(args);
 return (count);
 }
